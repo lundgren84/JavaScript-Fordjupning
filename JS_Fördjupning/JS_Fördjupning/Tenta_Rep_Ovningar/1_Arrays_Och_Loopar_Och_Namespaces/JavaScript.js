c227535e3c7210eb83c3d105ed05b882e7492aa3
lundgren84 <lundgren84@hotmail.se>
2017-01-10 07:46:44 +0100

    asd

--- JS_Fördjupning/JS_Fördjupning/Tenta_Rep_Ovningar/1_Arrays_Och_Loopar_Och_Namespaces/JavaScript.js
+++ JS_Fördjupning/JS_Fördjupning/Tenta_Rep_Ovningar/1_Arrays_Och_Loopar_Och_Namespaces/JavaScript.js
@@ -1,15 +1,21 @@
 ﻿$(document).ready(function () {
-    // Divs
+    // --Divs--
     var listPersonDiv = $('#listPersonDiv');
     var searchPersonDiv = $('#searchPersonDiv');
     var personFormDiv = $('#personFormDiv');
     listPersonDiv.hide(); searchPersonDiv.hide(); personFormDiv.hide();
-    // Buttons
-    var btn_list = $('#btn_list');
-    var btn_sreach = $('#btn_sreach');
-    var btn_add = $('#btn_add');
 
-    // Person array
+    var searchResultDiv = $('#searchResultDiv');
+    // --Buttons--
+    var btn_listDiv = $('#btn_listDiv');
+    var btn_sreachDiv = $('#btn_sreachDiv');
+    var btn_addDiv = $('#btn_addDiv');
+
+    var btn_searchPerson = $('#btn_searchPerson');
+    var btn_GetLastSearch = $('#btn_GetLastSearch');
+    // --Inputs--
+    var txt_searchText = $('#txt_searchText');
+    // --Person array--
     var personArray = [
         { fName: "Hampus", lName: "Lundgren", age: 32, city: "Helsingborg", fullName: function () { return this.fName + ' ' + this.lName; } },
         { fName: "Olle", lName: "Svensson", age: 12, city: "Oslo", fullName: function () { return this.fName + ' ' + this.lName; } },
@@ -17,19 +23,45 @@
         { fName: "Anja", lName: "Persson", age: 35, city: "Umeå", fullName: function () { return this.fName + ' ' + this.lName; } }];
 
     // Clicks
-    btn_list.click(function () {     
+    btn_listDiv.click(function () {
         personFormDiv.hide();
         listPersonDiv.html(listAllPersons);
         listPersonDiv.toggle(200);
     });
-    btn_sreach.click(function () {
+    btn_sreachDiv.click(function () {
         searchPersonDiv.toggle(200);
     });
-    btn_add.click(function () {
+    btn_addDiv.click(function () {
         listPersonDiv.hide();
         personFormDiv.toggle(200);
     });
+    btn_searchPerson.click(function () {
+        searchResultDiv.html(listSearchResult);
+        searchResultDiv.hide();
+        searchResultDiv.toggle();
+
+    });
+    btn_GetLastSearch.click(function () {
+        txt_searchText.val(localStorage.lastSearch);
+        searchResultDiv.html(listSearchResult);
+        searchResultDiv.hide();
+        searchResultDiv.toggle();
+    });
     // functions
+    function listSearchResult() {
+        let htmlContent = "";
+        let searchInput = txt_searchText.val();
+        localStorage.lastSearch = searchInput;
+        for (var i = 0; i < personArray.length; i++) {
+            if (searchInput.toLowerCase() === personArray[i].fName.toLowerCase() || searchInput.toLowerCase() === personArray[i].lName.toLowerCase()) {
+                let index = i + 1;           
+                    htmlContent += index + " - Name: " + personArray[i].fullName() + " - Age: " + personArray[i].age + " - City: " + personArray[i].city + "</br>";         
+            }
+        }
+
+        return htmlContent;
+    }
+
     function listAllPersons() {
         let htmlContent = "<h2>List Persons</h2>";
         for (var i = 0; i < personArray.length; i++) {
